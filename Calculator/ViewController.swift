aa95e5f26ccd0334998486d3e1f879104f791c63
markhao <mark.q.hao@gmail.com>
2016-06-09 11:56:56 -0700

    Implemented touchDigit and performOperation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    private var isInTheMiddleOfTyping = false
+    
+    @IBOutlet private weak var displayLabel: UILabel!
+    
+    @IBAction private func digitPressed(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = displayLabel.text!
+            displayLabel.text = textCurrentlyInDisplay + digit
+        } else {
+            displayLabel.text! = digit
+        }
+        isInTheMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var displayValue : Double {
+        get {
+            return Double(displayLabel.text!)!
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
     }
-
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        isInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            
+        }
+    }
+    
 
 }
 
